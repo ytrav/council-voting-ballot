7afe9205556e09f4d011dad44f128cc82e13cb7d
ytrav <withertrav394@gmail.com>
2024-08-24 01:46:02 +0200

    added parties, discord integration

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,18 +4,43 @@ import { useLocaleStore } from '../stores/localeStore.ts'
 import { mapStores } from 'pinia'
 
 export default {
+    data() {
+        return {
+            membersOnline: 0,
+        }
+    },
     components: {
         RouterLink
     },
     computed: {
         ...mapStores(useLocaleStore, ['locale'])
     },
+    mounted() {
+        fetch('https://discord.com/api/guilds/1240273703158419456/widget.json')
+            .then(response => response.json())
+            .then(data => {
+                this.membersOnline = data.members.length
+            }
+        )
+        setInterval(() => {
+            fetch('https://discord.com/api/guilds/1240273703158419456/widget.json')
+                .then(response => response.json())
+                .then(data => {
+                    this.membersOnline = data.members.length
+                }
+            )
+        }, 30000)
+    },
+    beforeUnmount() {
+        clearInterval(this.membersOnline)
+    }
 }
 </script>
 
 <template>
     <header>
         <h2>{{ localeStore.activeLocale.header.heading }}</h2>
+        <h3>Citizens {{ localeStore.activeLocale.header.online }}: {{ membersOnline }}</h3>
         <div class="lang-button">
             <RouterLink to="/eng" replace>ENG</RouterLink>
             <span>/</span>
--- src/views/AppLanding.vue
+++ src/views/AppLanding.vue
@@ -1,5 +1,5 @@
-<script>
-import { useLocaleStore } from '../stores/localeStore.ts'
+<script lang="ts">
+import { useLocaleStore } from '../stores/localeStore'
 import { mapStores } from 'pinia'
 
 export default {
@@ -17,6 +17,48 @@ export default {
                     label: 'Discord username:',
                     placeholder: '@johndoes',
                     variable: 'discordUsername'
+                }
+            ],
+            parties: [
+                {
+                    name: 'Costco Party',
+                    leader: 'Mr. Cash Money',
+                    banner: '/fridge_banner.webp',
+                    motto: 'We\'re the Costco Guys',
+                    desc: 'Everyone should have access to bulk goods at a low price, with the only large fee being a membership.',
+                    colours: ['e32a36', 'ffffff', '0060a9']
+                },
+                {
+                    name: 'Cross Byte\'s Party',
+                    leader: 'Cross_Byte',
+                    banner: '/cross_banner.jpeg',
+                    motto: 'Play toys with us',
+                    desc: 'The goal of my political party is to have fun and be silly but also make good decisions powered by the users.',
+                    colours: ['ffd700', '000000']
+                },
+                {
+                    name: 'Eighty the Best Party',
+                    leader: 'Eighty',
+                    banner: '/eighty_banner.jpg',
+                    motto: 'You should vote me NOW',
+                    desc: 'Givin rice and kisses to everybody.',
+                    colours: ['fec5e5']
+                },
+                {
+                    name: 'Maevesoupia Party, the Peak of Human Evolution and Doge Man',
+                    leader: 'Noobish',
+                    members: ['Doge Man', 'Serbyro'],
+                    banner: '/noobish_banner.PNG?url',
+                    motto: 'We need more soup!!!!!!',
+                    desc: 'We will make everyone lots of soup, and allow them to buy clothes from the soup store. (We will also reinstate the death penalty.) Oh and also we’re gonna make Homelander president of Jordan for funsies.<br><br>We think that soup will improve the living conditions of citizens, and if living conditions are nice, we can invest more time into other matters, like taking funny pictures of animals.<br><br>Speaking of animals, all octopuses must be protected and named either Timothy or Ambrosius. If an octopus appears, you are legally obligated to adopt it.<br><br>There must be a poster of The Deep from popular Amazon series \'The Boys\' in every official government building.<br>',
+                    colours: ['dc4601', '757816']
+                },
+                {
+                    name: 'Twink Suffrage Party',
+                    leader: 'Marceline the Vampire Queen',
+                    banner: '/marceline_banner.png',
+                    motto: 'Universal twink rights for all.',
+                    desc: 'I promise to bring about a free open democracy for all. Where all people not just twinks get their deserved rights. A world where food, shelter, and water are a common thing to find. I want to establish the great dining hall where you can post amazing looking food or talk about recipes for food. Now I am open to the people of this great country and if any of you have concerns I will cater to them.'
                 }
             ],
             enteredData: {
@@ -45,7 +87,32 @@ export default {
 
                 {{ localeStore.activeLocale.landing.soon }}
             </p>
+
+            <h2>Meet the parties of Maevetopia</h2>
+            <div class="party" v-for="(party, index) in parties" :key="index">
+                <h3>{{ index+1 }}. <span class="colour" v-for="colour in party.colours" :key="colour" :style="`background-color: #${colour}`"></span> {{ party.name }}</h3>
+                <h4>Headed by: <strong>{{ party.leader }}</strong> <span v-if="party.members">
+                    ({{ party.members.join(', ') }})
+                </span></h4>
+                <img :src="party.banner" :alt="party.name + ' banner'">
+                <p v-html="party.desc"></p>
+                    <h4>"{{party.motto}}"</h4> - {{party.leader}}
+               
+            </div>
+            <!-- <p>-----</p> -->
+
+            </div>
+            <div class="form">
+            
             <p>{{ localeStore.activeLocale.landing.soon2 }}<br>- Administration</p>
+            <p>Join Maevetopia to keep up with the election and participate in other exciting activities, as well as meet great people✨</p>
+
+            <a href="http://discord.maevetopia.fun" class="discord-button"><img src="/discord.png" alt="discord logo">{{ localeStore.activeLocale.landing.become }} a Citizen today</a>
+
+
+            <iframe src="https://discord.com/widget?id=1240273703158419456&theme=dark" width="350" height="500"
+                allowtransparency="true" frameborder="0"
+                sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
             <div class="input-field" v-for="field in fields" :key="field">
                 <label :for="field.name">{{ field.label }}</label>
                 <input type="text" :id="field.name" :name="field.name" :placeholder="field.placeholder"
@@ -53,5 +120,9 @@ export default {
                 <span>{{ enteredData[field.variable] }}</span>
             </div>
         </div>
+
+  <footer>
+    <span>© 2024 Maevetopia</span>
+  </footer>
     </div>
 </template>
